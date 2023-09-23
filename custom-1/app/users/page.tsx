35c78767cf0446dad7de06c94021423478fadce8
khairi <khairi.yusoff@qumonintelligence.com>
2023-09-23 14:43:50 +0800

    data fetch

--- custom-1/app/users/page.tsx
+++ custom-1/app/users/page.tsx
@@ -1,11 +1,23 @@
 import React from 'react'
 import ProductCard from '../components/ProductCard'
 
-const UsersPage = () => {
+interface User {
+  id:number,
+  name: string
+}
+
+const UsersPage = async () => {
+  
+  const res = await fetch('https://jsonplaceholder.typicode.com/users')
+  const users: User[] = await res.json()
+  console.log(users)
+
   return (
     <>
-    <div>UsersPage</div>
-    <ProductCard/>
+    <h1>Users</h1>
+    <ul>
+      {users.map(user=><li key={user.id}>{user.name}</li>)}
+    </ul>
     </>
 
   )
