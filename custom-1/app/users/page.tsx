4791463b6639d4c7f233e8e8ac454fc0d8eb88f9
khairi <khairi.yusoff@qumonintelligence.com>
2023-10-19 23:29:15 +0800

    update

--- custom-1/app/users/page.tsx
+++ custom-1/app/users/page.tsx
@@ -9,7 +9,7 @@ interface User {
 const UsersPage = async () => {
   
   const res = await fetch('https://jsonplaceholder.typicode.com/users', {
-    cache: 'no-store'
+    next: {revalidate:10}
   })
   const users: User[] = await res.json()
   console.log(users)
