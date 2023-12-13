16a25be3e685aebd45ce8b8e1325810271af0453
Mohd Khairi Mohd Yusoff <102360814+KhairiYusoff@users.noreply.github.com>
2023-12-14 01:28:28 +0800

    Added required for all form fields

--- blog/src/app/dashboard/page.jsx
+++ blog/src/app/dashboard/page.jsx
@@ -104,30 +104,31 @@ const Dashboard = () => {
           {isLoading
             ? "loading"
             : data?.map((post) => (
-                <div className={styles.post} key={post._id}>
-                  <div className={styles.imgContainer}>
-                    <Image src={post.img} alt="" width={200} height={100} />
-                  </div>
-                  <h4 className={styles.postTitle}>{post.title}</h4>
-                  <span
-                    className={styles.delete}
-                    onClick={() => handleDelete(post._id)}
-                  >
-                    DELETE
-                  </span>
+              <div className={styles.post} key={post._id}>
+                <div className={styles.imgContainer}>
+                  <Image src={post.img} alt="" width={200} height={100} />
                 </div>
-              ))}
+                <h4 className={styles.postTitle}>{post.title}</h4>
+                <span
+                  className={styles.delete}
+                  onClick={() => handleDelete(post._id)}
+                >
+                  DELETE
+                </span>
+              </div>
+            ))}
         </div>
         <form className={styles.new} onSubmit={handleSubmit}>
           <h1>Add New Post</h1>
-          <input type="text" placeholder="Title" className={styles.input} />
-          <input type="text" placeholder="Desc" className={styles.input} />
-          <input type="text" placeholder="Image" className={styles.input} />
+          <input type="text" placeholder="Title" className={styles.input} required />
+          <input type="text" placeholder="Desc" className={styles.input} required />
+          <input type="text" placeholder="Image" className={styles.input} required />
           <textarea
             placeholder="Content"
             className={styles.textArea}
             cols="30"
             rows="10"
+            required
           ></textarea>
           <button className={styles.button}>Send</button>
         </form>
