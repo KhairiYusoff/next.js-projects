130721525d76ff2415d0c55c027416616b622224
KHAIRI <mkhairiyusoff.1985@gmail.com>
2023-12-15 01:24:50 +0800

    Update

--- blog/src/app/layout.js
+++ blog/src/app/layout.js
@@ -1,7 +1,7 @@
-import Navbar from "@/components/Navbar/Navbar";
+import Navbar from "../components/Navbar/Navbar";
 import "./globals.css";
 import { Inter, Roboto, Poppins } from "next/font/google";
-import Footer from "@/components/Footer/Footer";
+import Footer from "../components/Footer/Footer";
 import { ThemeProvider } from "@/context/ThemeContext";
 import AuthProvider from "@/components/AuthProvider/AuthProvider";
 
