8924d8dc472768ac4c47e54839a4b1a08319b883
KHAIRI <mkhairiyusoff.1985@gmail.com>
2023-12-16 03:10:01 +0800

    Update

--- blog/src/app/portfolio/page.jsx
+++ blog/src/app/portfolio/page.jsx
@@ -10,7 +10,7 @@ export const metadata = {
 const Portfolio = () => {
     return (
         <div className={styles.container}>
-            <h1 className={styles.selectTitle}>Choose a gallery</h1>
+            <h1 className={styles.selectTitle}>Choose a gallery:</h1>
             <div className={styles.items}>
                 <Link href="/portfolio/illustrations" className={styles.item}>
                     <span className={styles.title}>Illustrations</span>
