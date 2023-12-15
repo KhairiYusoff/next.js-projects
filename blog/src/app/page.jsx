3d6ae419aecdfa83c9f05e2cea4fbcd7d6bf1816
KHAIRI <mkhairiyusoff.1985@gmail.com>
2023-12-16 01:37:04 +0800

    Update Home

--- blog/src/app/page.jsx
+++ blog/src/app/page.jsx
@@ -11,8 +11,7 @@ export default function Home() {
           Better design for your digital products.
         </h1>
         <p className={styles.desc}>
-          Turning your Idea into Reality. We bring together the teams from the
-          global tech industry.
+          Turning your Idea into Reality.
         </p>
         <Button url="/portfolio" text="See Our Works" />
       </div>
