fb6746c35059c749ae3064eb07a4188bed37d7ce
KHAIRI <mkhairiyusoff.1985@gmail.com>
2023-12-25 12:55:31 +0800

    Update media screen for contact page

--- blog/src/app/contact/page.jsx
+++ blog/src/app/contact/page.jsx
@@ -4,37 +4,39 @@ import Image from "next/image";
 import Button from "@/components/Button/Button";
 
 export const metadata = {
-    title: "Contact",
-    description: "Reach out for collaborations or inquiries. Let's connect to discuss opportunities, ideas, and feedback for my blog. I'm here to engage with readers and fellow bloggers alike.",
+  title: "Contact",
+  description:
+    "Reach out for collaborations or inquiries. Let's connect to discuss opportunities, ideas, and feedback for my blog. I'm here to engage with readers and fellow bloggers alike.",
 };
 
 const Contact = () => {
-    return (
-        <div className={styles.container}>
-            <h1 className={styles.title}>Lets Keep in Touch</h1>
-            <div className={styles.content}>
-                <div className={styles.imgContainer}>
-                    <Image
-                        src="/contact.png"
-                        alt=""
-                        fill={true}
-                        className={styles.image}
-                    />
-                </div>
-                <form className={styles.form}>
-                    <input type="text" placeholder="name" className={styles.input} />
-                    <input type="text" placeholder="email" className={styles.input} />
-                    <textarea
-                        className={styles.textArea}
-                        placeholder="message"
-                        cols="30"
-                        rows="10"
-                    ></textarea>
-                    <Button url="#" text="Send" />
-                </form>
-            </div>
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>Lets Keep in Touch</h1>
+      <div className={styles.content}>
+        <div className={styles.imgContainer}>
+          <Image
+            src="/contact.png"
+            alt=""
+            className={styles.image}
+            width={600}
+            height={500}
+          />
         </div>
-    );
+        <form className={styles.form}>
+          <input type="text" placeholder="name" className={styles.input} />
+          <input type="text" placeholder="email" className={styles.input} />
+          <textarea
+            className={styles.textArea}
+            placeholder="message"
+            cols="30"
+            rows="10"
+          ></textarea>
+          <Button url="#" text="Send" />
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Contact;
