f5d3937471ea53ecd73c7c51a449e3520191df96
KHAIRI <mkhairiyusoff.1985@gmail.com>
2024-01-08 21:26:08 +0800

    Update globals.css

--- eventapp/app/page.tsx
+++ eventapp/app/page.tsx
@@ -1,21 +1,12 @@
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
-import AKU from "../public/assets/images/hero.png";
 
 export default function Home() {
   return (
-    <>
-      <header className="p-6 bg-green-400 justify-between items-center flex">
-        <a href="">TEST</a>
-        <a href="">TEST</a>
-        <a href="">TEST</a>
-        <a href="">TEST</a>
-        <a href="">TEST</a>
-        <Button>Login</Button>
-      </header>
-      <div className="p-6 flex justify-center items-center">
-        <Image src={AKU} alt="" />
-      </div>
-    </>
+    <main>
+      <Button variant="destructive" className="px-2">
+        Hello
+      </Button>
+    </main>
   );
 }
