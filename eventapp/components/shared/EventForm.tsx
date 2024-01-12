4cea8d68b917614517285848c14096c1005c1279
Mohd Khairi Mohd Yusoff <102360814+KhairiYusoff@users.noreply.github.com>
2024-01-13 03:57:18 +0800

    Update

--- eventapp/components/shared/EventForm.tsx
+++ eventapp/components/shared/EventForm.tsx
@@ -69,6 +69,9 @@ const EventForm = ({ userId, type, event, eventId }: EventFormProps) => {
           path: '/profile'
         })
 
+        console.log(newEvent)
+
+
         if (newEvent) {
           form.reset();
           router.push(`/events/${newEvent._id}`)
