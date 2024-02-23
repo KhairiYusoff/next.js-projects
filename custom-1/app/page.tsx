9dc975a44530da4ff61fdbc25838dc978140a805
KHAIRI <mkhairiyusoff.1985@gmail.com>
2024-02-23 10:30:39 +0800

    clean up folders

--- custom-1/app/page.tsx
+++ custom-1/app/page.tsx
@@ -1,13 +1,7 @@
-import Image from "next/image";
-import Link from "next/link";
-import ProductCard from "./components/ProductCard";
-import MyImage from "./components/MyImage";
-
 export default function Home() {
   return (
     <main className="">
       <h1>hello world</h1>
-      <MyImage />
     </main>
   );
 }
