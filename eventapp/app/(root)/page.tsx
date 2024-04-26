a37d6a49b0ddd2029a3b2aa826fb0b85b7f42823
KHAIRI <mkhairiyusoff.1985@gmail.com>
2024-04-27 00:55:12 +0800

    Update hero image and text

--- eventapp/app/(root)/page.tsx
+++ eventapp/app/(root)/page.tsx
@@ -23,26 +23,28 @@ export default async function Home({ searchParams }: SearchParamProps) {
     <>
       <section className="bg-primary-50 bg-dotted-pattern bg-contain py-5 md:py-10">
         <div className="wrapper grid grid-cols-1 gap-5 md:grid-cols-2 2xl:gap-0">
-          <div className="flex flex-col justify-center gap-8">
+          <div className="flex flex-col justify-center gap-8 mr-4">
             <h1 className="h1-bold">
               Host, Connect, Celebrate: Your Events, Our Platform!
             </h1>
             <p className="p-regular-20 md:p-regular-24">
-              Book and learn helpful tips from 3,168+ mentors in world-class
-              companies with our global community.
+              Elevate your learning experience by connecting with experts from
+              world-class organizations, all within our dynamic global
+              community.
             </p>
             <Button size="lg" asChild className="button w-full sm:w-fit">
               <Link href="#events">Explore Now</Link>
             </Button>
           </div>
-
-          <Image
-            src="/assets/images/hero.png"
-            alt="hero"
-            width={1000}
-            height={1000}
-            className="max-h-[70vh] object-contain object-center 2xl:max-h-[50vh]"
-          />
+          <div className="flex justify-center">
+            <Image
+              src="/assets/images/hero2.jpg"
+              alt="hero"
+              width={500}
+              height={1000}
+              className="max-h-[70vh] object-contain object-center 2xl:max-h-[50vh]"
+            />
+          </div>
         </div>
       </section>
 
