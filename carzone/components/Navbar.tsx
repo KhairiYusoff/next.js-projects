14490cec4947151d33ffdcbd2b95f820c34b24fd
Mohd Khairi Mohd Yusoff <102360814+KhairiYusoff@users.noreply.github.com>
2024-07-06 09:05:09 +0800

    Update layout, navbar and global.css

--- carzone/components/Navbar.tsx
+++ carzone/components/Navbar.tsx
@@ -1,26 +1,28 @@
-import Image from "next/image";
 import Link from "next/link";
-import React from "react";
-import { CustomButton } from ".";
+import Image from "next/image";
 
-const Navbar = () => {
-  return (
-    <header className="p-4">
-      <nav className="flex justify-between items-center">
-        <div>
-          <Link href="/">
-            <Image src="/logo.svg" width={100} height={100} alt="logo" />
-          </Link>
-        </div>
-        <div>
-          {/* <button className="px-6 py-3 bg-blue-600 rounded-full text-white">
-            Sign In
-          </button> */}
-          <CustomButton title="Sign In" />
-        </div>
-      </nav>
-    </header>
-  );
-};
+import CustomButton from "./CustomButton";
 
-export default Navbar;
+const NavBar = () => (
+  <header className='w-full absolute z-10'>
+    <nav className='max-w-[1440px] mx-auto flex justify-between items-center sm:px-16 px-6 py-4 bg-transparent'>
+      <Link href='/' className='flex justify-center items-center'>
+        <Image
+          src='/logo.svg'
+          alt='logo'
+          width={118}
+          height={18}
+          className='object-contain'
+        />
+      </Link>
+
+      <CustomButton
+        title='Sign in'
+        btnType='button'
+        containerStyles='text-white rounded-full bg-white min-w-[130px] bg-blue-500'
+      />
+    </nav>
+  </header>
+);
+
+export default NavBar;
