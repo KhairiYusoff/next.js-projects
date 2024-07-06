f4898a0b0bafb26107c8622339f9a0cafd4dda7a
Mohd Khairi Mohd Yusoff <102360814+KhairiYusoff@users.noreply.github.com>
2024-07-06 08:42:55 +0800

    Update

--- carzone/components/index.ts
+++ carzone/components/index.ts
@@ -2,7 +2,6 @@ import Navbar from "./Navbar";
 import Footer from "./Footer";
 import Hero from "./Hero";
 import CustomButton from "./CustomButton";
-import CustomButton from "./CustomButton";
 import SearchBar from "./SearchBar";
 import SearchManufacturer from "./SearchManufacturer";
 
